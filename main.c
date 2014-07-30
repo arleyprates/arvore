c1a1dca09005b60834a5c849e08d691e35c3be06
Arley Prates <arleyprates@gmail.com>
2014-07-30 08:51:54 -0300

    Criando arovre e registro

--- main.c
+++ main.c
@@ -9,18 +9,64 @@ typedef struct node {
 	struct node *no_irmao;
 	struct node *no_conjuge;
 	struct node *no_filho;
-} No;
+} Registro;
 
 typedef struct arvore {
 	char nome[10];
-	No* no;
+	Registro *no;
 	struct arvore *esquerda;
 	struct arvore *direita;
 } Arvore;
+
+void inserirDados (Arvore *A1) {
+	char nome[10];
+	char sexo[3];
+	printf("Digite o nome da pessoa:\n");
+	gets (nome);
+	strcpy (A1->nome, nome);
+	strcpy (A1->no->nome, nome);
+	printf("Digite o sexo da pessoa:\n");
+	gets (sexo);
+	strcpy (A1->no->sexo, sexo);
+	printf("RETORNO2\n");	
+}
+
+Registro* criarRegistro () {
+	Registro *novo;
+	novo = (Registro*) malloc (sizeof(Registro));
+	if (novo == NULL) {
+		printf("RETORNO2 - ALOCACAO\n");
+		return 0;
+	}
+	return novo;
+}
+
+Arvore* criarNo () {
+	Arvore* novo;
+	novo = (Arvore*) malloc (sizeof(Arvore));
+	if (novo == NULL) {
+		printf("RETORNO2 - ALOCACAO\n");
+		return 0;
+	}
+	return novo;
+}
+int registrar (Arvore *A1) {
+	Arvore *novo;
+	novo = criarNo ();
+	novo->no = criarRegistro ();
+	inserirDados (novo);
+	printf("RETORNO1\n");
+	return 1;
+}
 
 int main () {
 	Arvore *A1, *A2;
 	A1 = NULL;
 	A2 = NULL;
+	printf("=============================\n");
+	printf("Menu\n");
+	printf("1 - Registrar uma pessoa\n");
+	registrar (A1);
+	printf("=============================\n");
 	return 0;
 }
