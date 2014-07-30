72ca577dee2c3309dfb378132323d8ff80acdfe0
Arley Prates <arleyprates@gmail.com>
2014-07-30 13:06:44 -0300

    Ajuste no casamento para impressao

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 typedef struct node {
-	char nome[10];
+	char nome[20];
 	char sexo[3];
 	struct node *no_pai;	
 	struct node *no_irmao;
@@ -12,7 +12,7 @@ typedef struct node {
 } Registro;
 
 typedef struct arvore {
-	char nome[10];
+	char nome[20];
 	Registro *no;
 	struct arvore *esquerda;
 	struct arvore *direita;
@@ -75,6 +75,14 @@ int casamento (Arvore **A1, Arvore **A2) {
 	Arvore *novo;
 	novo = criarNo ();
 	novo->no = criarRegistro ();
+	strcpy (novo->nome, "cas ");
+	strcpy (novo->no->nome,  "cas ");
+	strcat (novo->nome, (*A1)->nome);
+	strcat (novo->no->nome,  (*A1)->nome);
+	strcat (novo->nome, " e ");
+	strcat (novo->no->nome,  " e ");
+	strcat (novo->nome, (*A2)->nome);
+	strcat (novo->no->nome,  (*A2)->nome);
 	(*A1)->direita = novo;
 	(*A2)->esquerda = novo;	
 	printf("RETORNO3\n");
@@ -116,9 +124,9 @@ int main () {
 	printf("A1->no->sexo %s\n", A1->no->sexo);
 	printf("A2->no->nome %s\n", A2->no->nome);
 	printf("A2->no->sexo %s\n", A2->no->sexo);
-	printf("\n\n\n");
-	printf("A1->nome %s\n", A1->direita->nome);
-	printf("A1->no->nome %s\n", A1->direita->no->sexo);/*
+	printf("\n\n\n Casamento\n");
+	printf("A1->->direita->nome %s\n", A1->direita->nome);
+	printf("A1->direita->no->nome %s\n", A1->direita->no->nome);/*
 	printf("A1->no->sexo %s\n", A2->no->sexo);
 	printf("A1->nome %s\n", A1->nome);
 	printf("A1->no->nome %s\n", A1->no->nome);
