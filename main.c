10e02e36c850fd375097993915417f36c9b424ce
Arley Prates <arleyprates@gmail.com>
2014-07-30 08:07:02 -0300

    nova implementacao bugfix struct node

--- main.c
+++ main.c
@@ -5,19 +5,22 @@
 typedef struct node {
 	char nome[10];
 	char sexo[3];
-	struct *no_pai;	
-	struct *no_irmao;
-	struct *no_conjuge;
-	struct *no_filho;
+	struct node *no_pai;	
+	struct node *no_irmao;
+	struct node *no_conjuge;
+	struct node *no_filho;
 } No;
 
 typedef struct arvore {
 	char nome[10];
 	No* no;
-	struct arvore *esquerda
+	struct arvore *esquerda;
 	struct arvore *direita;
 } Arvore;
 
 int main () {
+	Arvore *A1, *A2;
+	A1 = NULL;
+	A2 = NULL;
 	return 0;
 }
